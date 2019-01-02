5d4ec30658cb9c252d98300d193d9eb6213000da
Bastien Delbouys <dinomagma@hotmail.fr>
2019-01-02 21:56:55 +0100

    finishing tracking

--- BRR-website/body/track-list.php
+++ BRR-website/body/track-list.php
@@ -1,6 +1,4 @@
 <?php
-	require 'functions/session.php';
-	
 	$search = $_SESSION['bbr']['search-runner'];
 	$sort = "";
 	
@@ -8,7 +6,24 @@
 		$search = htmlspecialchars(trim($_POST['search']));
 		$_SESSION['bbr']['search-runner'] = $search;
 	}
-	?>
+		
+	if(isset($_GET['race']) && !empty($_GET['race'])) 
+	{
+		$id = $_GET['race'];
+		
+		if(does_race_exist($id)) {
+			$race = get_race($id);
+		}
+		
+		else {
+			header("Location:index.php?page=home");
+		}
+	}
+	
+	if(!isset($_GET['race'])) {
+		$race = get_active_race();
+	}
+?>
 
 <h2 class="page-title followed-title">Tracking Runner</h2>
 		
@@ -26,121 +41,130 @@
 			</span>
 		</div>
 	</form>
-		
-<h3 class="text-left"> Running Area </h3>
-<table class="table table-bordered table-striped table-condensed">           
-	<thead>
-		<tr>
-			<th>Distance</th>
-			<th>Bib</th>
-			<th>Name</th>
-			<th>Team</th>
-			<th>Elaspsed Time</th>
-			<th>Date & Time</th>
-		</tr>
-	</thead>
-	
-	<tbody>
-		<?php
-			foreach(runner_track($search, "run") as $race_runner) {	
-			if($race_runner->Station != "b827eba42979") {
-					$runner = get_runner($race_runner->Runner);
-					$timestamp = get_last_timestamp($race_runner->Runner, $race_runner->Race);
-					$team = get_race_runner_team($race_runner->Runner, $race_runner->Race);
-					$elapsed = get_total_elapsed_time($race_runner->Runner, $race_runner->Race);
-					?>	
-						<tr>
-							<td><?=$race_runner->Distance?></td>
-							<td><?=$race_runner->Bib?></td>
-							<td><?=$runner->FirstName." ".$runner->LastName?></td>
-							<td><?=$team->Name?></td>
-							<td>
-								<?php
-									if($elapsed != null)
-									{
-										echo $elapsed;
-									}
-									else
-									{
-										echo "-";
-									}
-								?>
-							</td>
-							<td>
-								<?php
-									if($timestamp != null)
-									{
-										echo $timestamp->Timestamp;
-									}
-									else
-									{
-										echo "-";
-									}
-								?>
-							</td>
-						</tr>
+
+
+<?php
+	if(exist_track($search, "run", $race->ID)) {
+?>		
+		<h3 class="text-left"> Running Area </h3>
+		<table class="table table-bordered table-striped table-condensed">           
+			<thead>
+				<tr>
+					<th>Distance</th>
+					<th>Bib</th>
+					<th>Name</th>
+					<th>Team</th>
+					<th>Elaspsed Time</th>
+					<th>Date & Time</th>
+				</tr>
+			</thead>
+			
+			<tbody>
 				<?php
-				}
-			}
-		?>
-	</tbody>
-</table>
-		
-<h3 class="text-left"> Resting Area </h3>
-<table class="table table-bordered table-striped table-condensed">           
-	<thead>
-		<tr>
-			<th>Distance</th>
-			<th>Bib</th>
-			<th>Name</th>
-			<th>Team</th>
-			<th>Elaspsed Time</th>
-			<th>Date & Time</th>
-		</tr>
-	</thead>
-	
-	<tbody>
-		<?php
-			foreach(runner_track($search, "rest") as $race_runner) {	
-				if($race_runner->Station == "b827eba42979") {
-					$runner = get_runner($race_runner->Runner);
-					$timestamp = get_last_timestamp($race_runner->Runner, $race_runner->Race);
-					$team = get_race_runner_team($race_runner->Runner, $race_runner->Race);
-					$elapsed = get_total_elapsed_time($race_runner->Runner, $race_runner->Race);
-					?>	
-						<tr>
-							<td><?=$race_runner->Distance?></td>
-							<td><?=$race_runner->Bib?></td>
-							<td><?=$runner->FirstName." ".$runner->LastName?></td>
-							<td><?=$team->Name?></td>
-							<td>
-								<?php
-									if($elapsed != null)
-									{
-										echo $elapsed;
-									}
-									else
-									{
-										echo "-";
-									}
-								?>
-							</td>
-							<td>
-								<?php
-									if($timestamp != null)
-									{
-										echo $timestamp->Timestamp;
-									}
-									else
-									{
-										echo "-";
-									}
-								?>
-							</td>
-						</tr>
+					foreach(runner_track($search, "run", $race->ID) as $race_runner) {
+						$runner = get_runner($race_runner->Runner);
+						$timestamp = get_last_timestamp($race_runner->Runner, $race_runner->Race);
+						$team = get_race_runner_team($race_runner->Runner, $race_runner->Race);
+						$elapsed = get_total_elapsed_time($race_runner->Runner, $race_runner->Race);
+						?>	
+							<tr class='clickable-row' data-href="index.php?page=runner&runner=<?=$race_runner->Runner?>&race=<?=$race_runner->Race?>">
+								<td><?=$race_runner->Distance?></td>
+								<td><?=$race_runner->Bib?></td>
+								<td><?=$runner->FirstName." ".$runner->LastName?></td>
+								<td><?=$team->Name?></td>
+								<td>
+									<?php
+										if($elapsed != null)
+										{
+											echo $elapsed;
+										}
+										else
+										{
+											echo "-";
+										}
+									?>
+								</td>
+								<td>
+									<?php
+										if($timestamp != null)
+										{
+											echo $timestamp->Timestamp;
+										}
+										else
+										{
+											echo "-";
+										}
+									?>
+								</td>
+							</tr>
+						<?php
+					}
+				?>
+			</tbody>
+		</table>
+<?php
+	}
+?>
+
+<?php
+	if(exist_track($search, "rest", $race->ID)) {
+?>		
+		<h3 class="text-left"> Resting Area </h3>
+		<table class="table table-bordered table-striped table-condensed">           
+			<thead>
+				<tr>
+					<th>Distance</th>
+					<th>Bib</th>
+					<th>Name</th>
+					<th>Team</th>
+					<th>Elaspsed Time</th>
+					<th>Date & Time</th>
+				</tr>
+			</thead>
+			
+			<tbody>
 				<?php
-				}
-			}
-		?>
-	</tbody>
-</table>
+					foreach(runner_track($search, "rest", $race->ID) as $race_runner) {
+						$runner = get_runner($race_runner->Runner);
+						$timestamp = get_last_timestamp($race_runner->Runner, $race_runner->Race);
+						$team = get_race_runner_team($race_runner->Runner, $race_runner->Race);
+						$elapsed = get_total_elapsed_time($race_runner->Runner, $race_runner->Race);
+						?>	
+							<tr class='clickable-row' data-href="index.php?page=runner&runner=<?=$race_runner->Runner?>&race=<?=$race_runner->Race?>">
+								<td><?=$race_runner->Distance?></td>
+								<td><?=$race_runner->Bib?></td>
+								<td><?=$runner->FirstName." ".$runner->LastName?></td>
+								<td><?=$team->Name?></td>
+								<td>
+									<?php
+										if($elapsed != null)
+										{
+											echo $elapsed;
+										}
+										else
+										{
+											echo "-";
+										}
+									?>
+								</td>
+								<td>
+									<?php
+										if($timestamp != null)
+										{
+											echo $timestamp->Timestamp;
+										}
+										else
+										{
+											echo "-";
+										}
+									?>
+								</td>
+							</tr>
+						<?php
+					}
+				?>
+			</tbody>
+		</table>
+<?php
+	}
+?>
--- BRR-website/functions/track_functions.php
+++ BRR-website/functions/track_functions.php
@@ -1,16 +1,48 @@
 <?php
 	/* TRACK FUNCTIONS */
 	
-	function runner_track($keyword, $area) 
+	function exist_track($keyword, $area, $race_id)
 	{
 		global $db;
 
-        $e = array();
-        $sql = "SELECT * FROM race_display";
+		$status = "Running";
+		
+		$e = array(
+			'race_id' => $race_id,
+			'status' => $status,
+			'station_id' => "b827eba42979"
+		);
+		
+		if($area == "run") {
+			$sql = "SELECT rr.Race, rr.Runner, rr.Bib, rr.Club, rr.TotalTime, (((t.Lap - 1) * 10) + s.LengthFromStart) AS Distance, t.Timestamp, t.Station
+					FROM race_runner AS rr, club AS c, runner AS r, timestamp AS t, station AS s
+					WHERE rr.Race = :race_id AND c.ID = rr.Club AND r.ID = rr.Runner AND rr.Status = :status
+					AND CONCAT(r.FirstName, ' ', r.LastName, ' ', c.Name, ' ', rr.Bib) LIKE '%{$keyword}%'
+					AND t.Runner = rr.Runner AND t.Race = rr.Race AND  t.Timestamp = 
+					(SELECT MAX(t.timestamp) FROM timestamp AS t WHERE t.Runner = rr.Runner AND t.Race = :race_id)
+					AND  t.Station = s.ID AND s.ID <> :station_id";
+		}
+		else {
+			$sql = "SELECT rr.Race, rr.Runner, rr.Bib, rr.Club, rr.TotalTime, (((t.Lap - 1) * 10) + s.LengthFromStart) AS Distance, t.Timestamp, t.Station
+					FROM race_runner AS rr, club AS c, runner AS r, timestamp AS t, station AS s
+					WHERE rr.Race = :race_id AND c.ID = rr.Club AND r.ID = rr.Runner AND rr.Status = :status
+					AND CONCAT(r.FirstName, ' ', r.LastName, ' ', c.Name, ' ', rr.Bib) LIKE '%{$keyword}%'
+					AND t.Runner = rr.Runner AND t.Race = rr.Race AND  t.Timestamp = 
+					(SELECT MAX(t.timestamp) FROM timestamp AS t WHERE t.Runner = rr.Runner AND t.Race = :race_id)
+					AND  t.Station = s.ID AND s.ID = :station_id";
+		}	
         $req = $db->prepare($sql);
         $req->execute($e);
 		
-		$race_id = $req->fetch()['Race'];
+        $exist = $req->rowCount($sql);
+		
+        return($exist);	
+	}
+	
+	function runner_track($keyword, $area, $race_id) 
+	{
+		global $db;
+
 		$status = "Running";
 		
         $e = array(
