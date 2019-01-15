defe604335e2b6721e40de8540ae98ae9707c3f1
Bastien Delbouys <dinomagma@hotmail.fr>
2019-01-15 13:12:54 +0100

    security update

--- BRR-website/functions/initialisation_functions.php
+++ BRR-website/functions/initialisation_functions.php
@@ -12,10 +12,9 @@
 	// Try to connect to the database.
 	try {
 		$db = new PDO(
-			"mysql:host=".$dbhost.";dbname=".$dbname, 
+			"mysql:host=".$dbhost.";dbname=".$dbname.";charset=utf8mb4",
 			$dbuser, 
-			$dbpassword, 
-			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+			$dbpassword
 		);
 	}
 	
